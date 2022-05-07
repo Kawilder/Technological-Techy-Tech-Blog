5658d6eb5c94386364bdc5607a337aded46a15cf
Kawilder <kevin_wilde564@yahoo.com>
2022-05-07 17:06:38 -0500

    filled out/changed all the routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const postRoutes = require('./selectedPost');
+// const postRoutes = require('./selectedPost');
 const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/post', postRoutes);
+// router.use('/post', postRoutes);
 router.use('/dashboard', dashboardRoutes);
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
+
